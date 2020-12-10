56e5128744699b16d4065adcc4da7f9c2a9ba7e7
Ubuntu <ubuntu@ip-172-31-10-206.us-east-2.compute.internal>
2020-12-10 12:36:54 +0000

    Add password reset

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_10_081245) do
+ActiveRecord::Schema.define(version: 2020_12_10_122154) do
 
   create_table "posts", force: :cascade do |t|
     t.string "name"
@@ -32,6 +32,8 @@ ActiveRecord::Schema.define(version: 2020_12_10_081245) do
     t.string "activation_digest"
     t.boolean "activated", default: false
     t.datetime "activated_at"
+    t.string "reset_digest"
+    t.datetime "reset_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -11,8 +11,10 @@ class UserMailerPreview < ActionMailer::Preview
   end
 
   # Preview this email at
-  # http://localhost:3000/rails/mailers/user_mailer/password_reset
+# https://2f142ca770734224a9292da9eb7ad155.vfs.cloud9.us-east-2.amazonaws.com/rails/mailers/user_mailer/password_reset  
   def password_reset
-    UserMailer.password_reset
+    user = User.first
+    user.reset_token = User.new_token
+    UserMailer.password_reset(user)
   end
 end
