ddf1ea6715046b0550fc021ea4fa31b964c98bdc
Ubuntu <ubuntu@ip-172-31-10-206.us-east-2.compute.internal>
2020-12-12 07:52:05 +0000

    Add user following

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+  end
+
 test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -9,4 +13,16 @@ test "layout links" do
     assert_select "a[href=?]", contact_path
     assert_select "a[href=?]", signup_path
   end
+
+  test "layout links when logged in user" do
+    log_in_as(@user)
+    get root_path
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_match @user.active_relationships.count.to_s, response.body
+    assert_match @user.passive_relationships.count.to_s, response.body
+  end
+
 end
